6dbe8db3f5432c4567915affd1acc708fd6cb044
Chris Busick <cbusick@gmail.com>
2016-12-29 10:23:50 -0800

    Add handling for post

--- web.js
+++ web.js
@@ -6,6 +6,10 @@ app.get('*', function(request, response) {
   response.redirect(process.env.NEW_BASE_URL + request.url)
 });
 
+app.post('*', function(request, response) {
+  response.redirect(process.env.NEW_BASE_URL + request.url)
+});
+
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
